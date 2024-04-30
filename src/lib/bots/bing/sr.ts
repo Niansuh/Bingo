fd692d0fc3e39cca555810cad1b9d6274cd269e4
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-04-30 17:10:40 +0500

    Update sr.ts

--- src/lib/bots/bing/sr.ts
+++ src/lib/bots/bing/sr.ts
@@ -24,7 +24,7 @@ export class SR {
     if (!this.recognition) {
       return
     }
-    this.configuration('zh-CN')
+    this.configuration('en-US')
     if (commands.length) {
       this.commandsRe = new RegExp(`^(${commands.join('|')})。?$`)
     }
@@ -56,7 +56,7 @@ export class SR {
     }
   }
 
-  private configuration = async (lang: string = 'zh-CN') => {
+  private configuration = async (lang: string = 'en-US') => {
     return new Promise((resolve) => {
       if (this.recognition) {
         this.recognition.continuous = true
