3a9ad27405cb494773df4b39d71f9566c3c5b230
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-08-15 04:26:07 -0400

    Update UserAgents

--- src/lib/bots/bing/index.ts
+++ src/lib/bots/bing/index.ts
@@ -193,7 +193,7 @@ export class BingWebBot {
   async createConversation(conversationId?: string): Promise<ConversationResponse> {
     const headers = {
       'Accept-Encoding': 'gzip, deflate, br, zsdch',
-      'x-ms-useragent': 'azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.12.3 OS/Android',
+      'x-ms-useragent': 'azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.15.1 OS/Android',
       cookie: this.cookie,
     }
 
@@ -351,7 +351,7 @@ export class BingWebBot {
         headers: {
           'accept-language': 'en-US,en;q=0.9',
           'cache-control': 'no-cache',
-          "user-agent": "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R11t) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36 EdgA/108.0.1462.4",
+          "user-agent": "Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36 Edg/127.0.0.0",
           pragma: 'no-cache',
           cookie: this.cookie,
         }
@@ -371,7 +371,7 @@ export class BingWebBot {
   private async createImage(prompt: string, id: string) {
     const headers = {
       'Accept-Encoding': 'gzip, deflate, br, zsdch',
-      'x-ms-useragent': 'azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.12.3 OS/Android',
+      'x-ms-useragent': 'azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.15.1 OS/Android',
       cookie: this.cookie,
     }
 
